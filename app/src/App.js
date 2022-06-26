2532f567174051f82eab38d3733bfdaa3fb46a2f
milliorn <scottmilliorn@gmail.com>
2022-06-26 14:45:24 -0700

    getPost

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,54 @@
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const endpointIP = axios.create({ baseURL: "https://api.ipify.org" });
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [ip, setIp] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    try {
+      async function getPost() {
+        const response = await endpointIP.get("/");
+        setIp(response.data);
+      }
+      getPost();
+    } catch (error) {
+      setError(error);
+    }
+  }, []);
+
+  if (!ip) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>IP is null.</h1>
+        </header>
+      </div>
+    );
+  } else if (error) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>IP is unavailable.</h1>
+        </header>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>{ip}</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
